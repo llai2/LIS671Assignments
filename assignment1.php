c87f9b6c4e5a73589c44f0207bcd10eaab59dc22
llai2 <llai2@pratt.edu>
2015-09-15 18:46:49 -0400

    Update assignment1.php

--- assignment1.php
+++ assignment1.php
@@ -49,7 +49,7 @@ $beer = 10;
 do
 {
 	echo "<br>";
-	echo $beer . " bottles of beer on the wall, " . $beer . " bottles of beer!";
+	echo $beer . " bottles of beer on the wall, " . ($beer-1) . " bottles of beer!";
 	--$beer;	
 } while ($beer > 0);
 echo "<br>";
